fc949b254b1798a911f7c9ffd8a1b70266a8a217
Sami Atoui <atoui79@gmail.com>
2022-12-08 18:01:17 -0600

    minor update

--- src/components/Repos/RepoList.js
+++ src/components/Repos/RepoList.js
@@ -5,7 +5,7 @@ export default function RepoList() {
     return (
         <div className="repo-main">
             <h1>Personal projects</h1>
-            <h4>This list is auto-generated from my public GitHub repos.</h4>
+            <h4>This list is auto-populated from my public GitHub repos.</h4>
             <div className="repo-container">
                 <Repos />
 
