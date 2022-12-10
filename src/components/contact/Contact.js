0eead20410fba8b06171516f1c70ea67056728f1
Sami Atoui <atoui79@gmail.com>
2022-12-10 11:55:22 -0600

    more css fixes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,9 +3,16 @@ import "./contact.css"
 export default function Contact() {
     return (
         <div className="contact">
-            <p>👤<a href="https://www.linkedin.com/in/samiatoui/" target="_blank">LinkedIn</a></p>
-            <p>✉️<a href="mailto:atoui79@gmail.com" target="_blank">atoui79@gmail.com</a></p> 
-            <p>⌨️<a href="https://github.com/samiatoui/" target="_blank">GitHub</a></p>
+            <div className="contact-item">
+                <a href="https://www.linkedin.com/in/samiatoui/" target="_blank">👤LinkedIn</a>
+            </div>
+            <div className="contact-item">
+                <a href="mailto:atoui79@gmail.com" target="_blank">✉️atoui79@gmail.com</a>
+            </div>
+            <div className="contact-item">
+                <a href="https://github.com/samiatoui/" target="_blank">⌨️GitHub</a>
+            </div>
+
         </div>
     )
 }
