497cafb9acd01e5f37b3507f7bc05e0e816d9681
Sami <atoui79@gmail.com>
2024-03-12 01:51:20 -0500

    banner fix

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,12 @@ import MenuItems from "./MenuItems";
 export default function About() {
     return (
         <div className="about">
-
-            <h1>Sami Atoui</h1>
-            <p>Software Developer</p>
-            <div className="about-btn">
-                <MenuItems />
+            <div className='about-text'>
+                <h1>Sami Atoui</h1>
+                <p>Software Developer</p>
+                <div className="about-btn">
+                    <MenuItems />
+                </div>
             </div>
         </div>
     )
