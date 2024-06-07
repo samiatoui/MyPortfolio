b9ca70ae64682ab4e593eb447f7d500ac329c985
sami <atoui79@gmail.com>
2024-06-07 13:47:01 -0500

    updated bio

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ export default function About() {
     return (
         <div className="about">
             <div className='about-text'>
-                <h1>Sami Atoui</h1>
-                <p>Software Developer</p>
+                <h1>Hi 👋, I'm Sami Atoui</h1>
+                <p>A Software Developer</p>
                 <div className="about-btn">
                     <MenuItems />
                 </div>
