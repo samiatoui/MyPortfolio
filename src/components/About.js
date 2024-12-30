07e6db7c7ae859b626c87a08db61cecb5e42b189
sami <atoui79@gmail.com>
2024-12-29 20:47:38 -0600

    updated bio and css

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,18 @@ export default function About() {
     return (
         <div className="about">
             <div className='about-text'>
-                <h1>Hi👋, I'm Sami Atoui</h1>
-                <p>A Software Developer</p>
+                <p style={{ fontSize: '18px', opacity: '0.5' }}>Hello! I am</p>
+                <p>Sami Atoui</p>
+                <p style={{ color: 'rgb(173, 0, 107)', fontSize: '18px' }}>Software Developer</p>
+                <br></br>
+                <p style={{ fontSize: '20px'}}>I’m a Software Developer from Winnipeg, Manitoba, Canada. I love building web and mobile apps that are both functional and fun to use. Whether it’s a full-time project or freelance work, I enjoy solving problems and bringing ideas to life with code.</p>
+                <br></br>
                 <div className="about-btn">
                     <MenuItems />
                 </div>
+            </div>
+            <div className='about-picture'>
+                <img src="https://i.imgur.com/urDP0gd.png"></img>
             </div>
         </div>
     )
